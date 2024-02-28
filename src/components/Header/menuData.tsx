c13ad35ed624e6a4ed3c0ced0024b7d74193552b
mkdev11 <homraming@gmail.com>
2024-02-28 13:06:58 -0500

    fatal: deleted Pages in menu

--- src/components/Header/menuData.tsx
+++ src/components/Header/menuData.tsx
@@ -24,49 +24,6 @@ const menuData: Menu[] = [
     title: "Contact Us",
     path: "/contact",
     newTab: false,
-  },
-  {
-    id: 4,
-    title: "Pages",
-    newTab: false,
-    submenu: [
-      {
-        id: 41,
-        title: "Terms of Service",
-        path: "/terms-of-service",
-        newTab: false,
-      },
-      {
-        id: 42,
-        title: "Contact Page",
-        path: "/contact",
-        newTab: false,
-      },
-      {
-        id: 43,
-        title: "Blog Grid Page",
-        path: "/blog",
-        newTab: false,
-      },
-      {
-        id: 44,
-        title: "Blog Sidebar Page",
-        path: "/blog-sidebar",
-        newTab: false,
-      },
-      {
-        id: 45,
-        title: "Blog Details Page",
-        path: "/blog-details",
-        newTab: false,
-      },
-      {
-        id: 48,
-        title: "Error Page",
-        path: "/error",
-        newTab: false,
-      },
-    ],
-  },
+  }
 ];
 export default menuData;
